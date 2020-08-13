b5fd35695996de59abf87b2d7b17969c7db82469
多田秀人 <shoot@Shootie-MacBook.local>
2020-08-13 15:04:41 +0900

    add css & js.

---
+++ diary/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+
+class InquiryForm(forms.Form):
+    name = forms.CharField(label='お名前', max_length=30)
+    email = forms.EmailField(label='メールアドレス')
+    title = forms.CharField(label='タイトル', max_length=30)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+
+    def __init__(self, *args, **kwards):
+        super().__init__(*args, **kwards)
+
+        self.fields['name'].widget.attrs['class'] = 'form-control col-9'
+        self.fields['name'].widget.attrs['placeholder'] = 'お名前をこちらに入力してください。'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['email'].widget.attrs['placeholder'] = 'メールアドレスをこちらに入力してください。'
+
+        self.fields['title'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをこちらに入力してください。'
+
+        self.fields['message'].widget.attrs['class'] = 'form-control col-12'
+        self.fields['message'].widget.attrs['placeholder'] = 'メッセージをこちらに入力してください。'
